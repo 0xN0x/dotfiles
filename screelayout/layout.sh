098eb244e6a0c9bc9330a474b24d3cb5378e51b3
0xN0x <0xNox@protonmail.com>
2022-10-26 17:36:49 +0200

    add: bspwm,picom,polybar,ranger,screenlayout,sxhkd,zsh

---
+++ screelayout/layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate right --output DP-0 --mode 1920x1080 --pos 4520x840 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 3440x1440 --pos 1080x480 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off
